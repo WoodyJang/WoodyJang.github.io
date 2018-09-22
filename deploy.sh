b7b0d3366d1e258f9422e73adc4bfbeda8828b8d
WoodyJang <Woodyjang@126.com>
2018-09-22 14:16:35 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io
-git push -f git@github.com:WoodyJang/WoodyJang.github.io.git vuepress
+git push -f https://github.com/WoodyJang/WoodyJang.github.io.git master
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
