ca5548c9f1f0a5ac184c08eb4a350839d7a34a32
zhangwu <zhangwu@chezhibao.com>
2018-09-22 13:50:51 +0800

    vuepress init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  title: "Ivan",
+  description: "记录生活，记录成长",
+  base: "/",
+  host: "0.0.0.0",
+  //mac下port未生效
+  port: 8081,
+
+  themeConfig: {
+    //gitc 仓库地址
+    repo: "WoodyJang/WoodyJang.github.io",
+    docsDir: "docs",
+    editLinks: true,
+    editLinkText: "在github上编辑本页",
+    //导航栏
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "技术博客", link: "/technical/" },
+      { text: "个人文章", link: "/article/" },
+      { text: "学习", link: "/study/" }
+    ],
+
+    //搜索
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: "Last Updated" // string | boolean
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+---
+home: true             
+actionText: 起步 →
+actionLink: /technical/
+footer: MIT Licensed | Copyright © 2018-present ZhangWu
+---
